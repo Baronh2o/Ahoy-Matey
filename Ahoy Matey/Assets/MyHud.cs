65908cc7844dffc1ca5da1fe297c226bf006d02d
Dustin Lunsford <lunsford.dustin@gmail.com>
2018-03-26 16:12:46 -0400

    replaced network manager

---
+++ Ahoy Matey/Assets/MyHud.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHud : MonoBehaviour {
+
+    private NetworkManager networkManager;
+
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+	}
+
+    public void MyStartHost() {
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+        networkManager.StartHost();
+    }
+
+    void OnStartHost() {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+}
