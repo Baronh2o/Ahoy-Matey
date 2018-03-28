19b0cdfa475bc207005b5816f07477434f627373
Dustin Lunsford <lunsford.dustin@gmail.com>
2018-03-28 14:16:07 -0400

    implemented networked player movement

---
+++ Ahoy Matey/Assets/Players/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+
+    // Update is called once per frame
+    void Update() {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        // input handling for local player only
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        inputValue.y = 0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.Translate(inputValue);
+	}
+}
